1a20eccf9fb9daa47ed8150788b0e71cb91a22c7
Jonathan <jonathan.ribeiro28121997@gmail.com>
2021-06-14 16:00:31 -0300

    First commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  foto:'',
+  token:'',
+  nome:'',
+  id:0,
+  server: 'https://jhowsblogpessoal.herokuapp.com'
+};
+
+// 'https://jhowsblogpessoal.herokuapp.com'
